95da987838d5303f04ed8cf9c5dbd967fffce413
roliygu <roliygu@gmail.com>
2013-05-19 01:04:43 +0800

    creat the project

---
+++ Analysis.py
@@ -0,0 +1,50 @@
+text1 = open('D:\Git\sliderAnalysis\oldoutput.txt')
+list3=text1.read().split('  ')
+so = set(list3)
+text1.close()
+
+text = open('D:\Git\sliderAnalysis\input.txt','r')
+list1 = text.readlines()
+list2=[]
+list4=[]
+list5=[]
+
+for i  in list1:
+    i=i[:-1]
+    k = i.split(' ')
+    for j  in k:
+        if 4 < len(j) < 12:
+            list2.append(j)
+
+for i in list2:
+    if i[-1]==',':
+        i = i[:-1]
+        list4.append(i)
+    elif i[-1]=='.':
+        i = i[:-1]
+        list4.append(i)
+    elif i[-1]=='?':
+        i = i[:-1]
+        list4.append(i)
+    elif i[-1]=='"':
+        i=i[:-1]
+        list4.append(i) 
+    elif i[0]=='"':
+        i=i[:-1]
+        list4.append(i) 
+    else:
+        list4.append(i)  
+        
+text.close()
+        
+s1 = set(list4)
+s3 = s1 - so
+out = open('D:\Git\sliderAnalysis\oldoutput.txt','w')
+for i in s1:
+    out.write(i)
+    out.write("  ")
+
+out.close()     
+   
+
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 sliderAnalysis
 ==============
+这个project主要功能是从字幕文件中划分出单词，并判断是新单词或旧单词
+input文件是新的字幕文件；
+oldoutput是已认识的单词库；
+output是input - oldouput的的生词库；
